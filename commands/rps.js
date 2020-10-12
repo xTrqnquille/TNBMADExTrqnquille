a101f5f5db0172f0193831305840f83b9666d3a0
Demian <demian.meens1@gmail.com>
2020-10-12 17:18:46 +0200

    fdsaf
    gsdfdg

--- commands/rps.js
+++ commands/rps.js
@@ -12,15 +12,15 @@ module.exports.run = async(client, message, args) => {
 
         if(result == "Schaar") {
 
-        return message.channel.send(`Ik Heb ${result} :scissors:, You WON GG!`);
+        return message.channel.send(`Ik Heb ${result} :scissors:, Jij wint GG!`);
 
     } else if(result == "Papier") {
 
-        return message.channel.send(`Ik Heb ${result} :notepad_spiral:, I WON GG!`);
+        return message.channel.send(`Ik Heb ${result} :notepad_spiral:, Ik win GG!`);
 
     } else if(result == "Steen") {
 
-        return message.channel.send(`Ik Heb ${result} :moyai:, it's a tie GG!`);
+        return message.channel.send(`Ik Heb ${result} :moyai:, Het is gelijk spel GG!!`);
 
         }
     
@@ -30,15 +30,15 @@ else if (args[0].toUpperCase() == "PAPIER") {
 
         if(result == "Papier") {
 
-        return message.channel.send(`Ik heb ${result} :notepad_spiral:, it's a tie GG!!`);
+        return message.channel.send(`Ik heb ${result} :notepad_spiral:, Het is gelijk spel GG!!`);
 
     } else if(result == "Schaar") {
 
-        return message.channel.send(`Ik heb ${result} :scissors:, I WON GG!`);
+        return message.channel.send(`Ik heb ${result} :scissors:, Ik win GG!`);
 
     } else if(result == "Steen") {
 
-        return message.channel.send(`Ik heb ${result} :moyai:, You WON GG!`);
+        return message.channel.send(`Ik heb ${result} :moyai:, Jij wint GG!`);
 
         }
     
