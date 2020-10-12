bf95a4fbefb0e759e617d73dcda06771ed00e14a
Demian <demian.meens1@gmail.com>
2020-10-12 16:54:28 +0200

    dasfd
    fdsfsd

--- commands/announcement.js
+++ commands/announcement.js
@@ -3,7 +3,7 @@ const discord = require("discord.js");
 module.exports.run = async (client, message, args) => {
  
  
-    if (!message.member.hasPermission("KICK_MEMBERS")) return message.reply("sorry you can't do that command");
+    if (!message.member.hasPermission("KICK_MEMBERS")) return message.reply("U moet Permissions hebben voor dat command!");
  
     var seperator = "|";
  
@@ -11,7 +11,7 @@ module.exports.run = async (client, message, args) => {
         var embed = new discord.MessageEmbed()
             .setTitle("Use")
             .setColor("#00ee00")
-            .setDescription(`Make an announcement by using: \n !announcement Title ${seperator} Messgage ${seperator} Color ${seperator} `);
+            .setDescription(`Maak een mededeling door gebruik te maken van: \n !mededeling Title ${seperator} Bericht ${seperator} Kleur ${seperator} `);
  
         return message.reply(embed);
     }
@@ -26,7 +26,7 @@ module.exports.run = async (client, message, args) => {
     var options = {
  
         titel: argsList[0],
-        bericht: argsList[1] || "No content specified",
+        bericht: argsList[1] || "Geen inhoud opgegeven",
         kleur: argsList[2].trim(),
         kanaal: argsList[3].trim()
  
@@ -37,17 +37,19 @@ module.exports.run = async (client, message, args) => {
     var announceEmbed = new discord.MessageEmbed()
         .setTitle(`${options.titel}`)
         .setColor(options.kleur)
+        .setThumbnail(`https://i.imgur.com/EU1zAXv.jpg`)
+        .setFooter(`© TeaNetwork`, `https://i.imgur.com/EU1zAXv.jpg`)        
         .setDescription(`${options.bericht}`)
-        .setFooter(`Message author: ${message.author.tag}`)
+        .setFooter(`Verstuurd door: ${message.author.tag}`)
         .setTimestamp();
  
     var channel = message.member.guild.channels.cache.find(channel => channel.name === options.kanaal);
-    if (!channel) return message.reply("Channel does not exist");
+    if (!channel) return message.reply("Kanaal bestaat niet,");
  
     channel.send(announceEmbed);
  
 }
  
 module.exports.help = {
-    name: "announcement"
+    name: "mededeling"
 }
--- commands/ban.js
+++ commands/ban.js
@@ -3,27 +3,27 @@ const discord = require ("discord.js")
 module.exports.run = async(client, message, args) => {
 
 
-    if (!message.member.hasPermission("BAN_MEMBERS")) return message.reply("You need Permissions for that command!");
+    if (!message.member.hasPermission("BAN_MEMBERS")) return message.reply("U moet Permissions hebben voor dat command!");
 
-    if (!message.guild.me.hasPermission("BAN_MEMBERS")) return message.reply("No Perms");
+    if (!message.guild.me.hasPermission("BAN_MEMBERS")) return message.reply("Geen Perms");
     
-    if (!args[1]) return message.reply("No user Selected.");
+    if (!args[1]) return message.reply("Geen members gegeven.");
 
-    if (!args[2]) return message.reply("No Reason Selected.");
+    if (!args[2]) return message.reply("Geen reden gegeven.");
 
     var banUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[1]));
 
     var reason = args.slice(2).join(" ");
 
-    if(!banUser) return message.reply("User not found");
+    if(!banUser) return message.reply("Persoon niet gevonden");
 
     var embedPrompt = new discord.MessageEmbed()
         .setColor("#8a2be2")
-        .setTitle("You got 30 Seconds to react")
-        .setThumbnail(`https://i.imgur.com/8V0b6RZ.jpg`)
+        .setTitle("U heeft 30 seconden om te reageren")
+        .setThumbnail(`https://i.imgur.com/EU1zAXv.jpg`)
         .setTimestamp()
-        .setFooter(`© xTrqn Development`, `https://i.imgur.com/8V0b6RZ.jpg`)
-        .setDescription(`Do you want to ban ${banUser}?`);
+        .setFooter(`© TeaNetwork`, `https://i.imgur.com/EU1zAXv.jpg`)
+        .setDescription(`Wil jij ${banUser} Bannen?`);
 
     var embed = new discord.MessageEmbed()
         .setColor("#8a2be2")
@@ -31,9 +31,9 @@ module.exports.run = async(client, message, args) => {
         .setTimestamp()
         .setThumbnail(`https://i.imgur.com/8V0b6RZ.jpg`)
         .setFooter(`© xTrqn Development`, `https://i.imgur.com/8V0b6RZ.jpg`)
-        .setDescription(`**Banned: ** ${banUser} (${banUser.id})
-        ** Banned By: ** ${message.author}
-        ** Reason: ** ${reason}`);
+        .setDescription(`**Gebanned: ** ${banUser} (${banUser.id})
+        ** Geband door: ** ${message.author}
+        ** Reden: ** ${reason}`);
 
     message.channel.send(embedPrompt).then(async msg => {
 
@@ -44,7 +44,7 @@ module.exports.run = async(client, message, args) => {
             msg.delete();
 
             banUser.ban(reason).catch(err => {
-                if (err) return message.reply("There is a Error!");
+                if (err) return message.reply("Er is een fout!");
             });
 
             message.channel.send(embed);
@@ -53,7 +53,7 @@ module.exports.run = async(client, message, args) => {
 
             msg.delete();
 
-            message.reply("Ban canceled").then(m => m.delete(5000));
+            message.reply("Ban Geannuleerd").then(m => m.delete(5000));
 
         }
     });
