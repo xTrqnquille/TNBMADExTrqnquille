5f44cdc7a3bdc9ed0659a6126ab90faa5388ede2
Demian <demian.meens1@gmail.com>
2020-11-16 17:19:35 +0100

    GVD12
    GVD1

--- commands/sollicitatie.js
+++ commands/sollicitatie.js
@@ -146,7 +146,7 @@ module.exports.run = async(client, message, args) => {
                                                         .setTitle("Bedankt voor de sollicitatie!")
                                                         .setColor("PURPLE")
                                                         .setTimestamp()
-                                                        .setDescription(`**Naam:** \n${antwoord1} \n**Leeftijd:** \n${antwoord2}\n**MineCraftNaam:** \n${antwoord3}\n**Functie:** \n${antwoord4}\n**WaaromJij:** \n${antwoord5}\n**Motivatie:** \n${antwoord6}\n**+/- Punten:** \n${antwoord8}\n**Verdereaanvullingen:** \n${antwoord9}\n**vragen:** \n${antwoord1}`);
+                                                        .setDescription(`**Naam:** \n${antwoord1} \n**Leeftijd:** \n${antwoord2}\n**MineCraftNaam:** \n${antwoord3}\n**Functie:** \n${antwoord4}\n**WaaromJij:** \n${antwoord5}\n**Motivatie:** \n${antwoord6}\n**+/- Punten:** \n${antwoord8}\n**Verdereaanvullingen:** \n${antwoord8}\n**vragen:** \n${antwoord8}`);
                                                     
                                                     settedParent.bulkDelete(14).then(
                                                         settedParent.send(uitkomst)
